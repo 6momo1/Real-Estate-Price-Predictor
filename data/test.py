6a435d2dd5fec948497cbcc318562496d3ce5e11
tomi <64671485+6momo1@users.noreply.github.com>
2021-01-11 00:14:39 -0800

    small change

---
+++ data/test.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+df = pd.read_csv("./raw data/data_vancouver_copy.csv",encoding = "ISO-8859-1")
+
+# print(df.area.unique())
+
+# vancouver west
+st = 'Champlain-Heights,Collingwood,Downtown-East,Fraser-East,Fraserview-East,Grandview-East,Hastings,Hastings-Sunrise,Killarney,Knight,Main,Mount-Pleasant-East,Renfrew,Renfrew-Heights,South-Vancouver,Victoria-East'
+
+st = st.split(',')
+
+for i in st:
+	print(f"<option value='{i}'>{i}</option>")
