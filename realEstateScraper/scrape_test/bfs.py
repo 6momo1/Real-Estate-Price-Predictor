a0f839e07cb64b0a2cc9beb90176a6cc6f4d2177
Tomi Lui <tomilui1221@gmail.com>
2021-05-15 17:29:20 -0700

    linux driver added

--- realEstateScraper/scrape_test/bfs.py
+++ realEstateScraper/scrape_test/bfs.py
@@ -31,28 +31,17 @@ class Utils:
 
         # get address
         address = soup.findAll("div", {"class": "listingheader-address"})
-
-        # save address to list
-        address_text = address[0].text
-        table_data.append(['address',address_text])
-
         # save address to dictionary 
-        # print('ADDRESS:',address_text, type(address_text))
         data_dic['address'] = address_text
 
 
         # get price
         price = soup.findAll("div", {"class": "listingheader-price"})
-
-        # save price to list
-        price_text = price[0].text
-        table_data.append(['price',price_text])
         # save price to dictionary
         data_dic['price'] = price_text
 
 
         all_tbody = soup.find_all('tbody')
-
         for tbody in all_tbody:
             for tr in tbody.find_all('tr'):
                 key = tr.find_all('th')[0].text.strip('\n')
--- realEstateScraper/scrape_test/main.py
+++ realEstateScraper/scrape_test/main.py
@@ -2,27 +2,27 @@ from rewScraper import Scraper
 from bfs import Utils
 
 # Interface
-# running = True
+running = True
 
-# while running:
-# 	area = input("Enter area: ").split(',')
-# 	city = input('Enter city: ')
-# 	pages = int(input("how many pages would you like to scrape?"))
+while running:
+	area = input("Enter area: ").split(',')
+	city = input('Enter city: ')
+	pages = int(input("how many pages would you like to scrape?"))
 
-# 	for a in area:
-# 		for p in range(1,pages+1):
-# 			try:
-# 				Scraper().scrapeRew(a,city,p)
-# 			except:
-# 				pass
+	for a in area:
+		for p in range(1,pages+1):
+			try:
+				Scraper().scrapeRew(a,city,p)
+			except:
+				pass
 
-# 	keep_running = input("Would you like to pass? (y/n)")
-# 	if keep_running == 'n':
-# 		running = False
+	keep_running = input("Would you like to pass? (y/n)")
+	if keep_running == 'n':
+		running = False
 
 
-# test Scraper().scrapeRew() function
-Scraper().scrapeRew('arbutus','vancouver', 1)
+# # test Scraper().scrapeRew() function
+# Scraper().scrapeRew('arbutus','vancouver', 1)
 
 
 # # test Utils().get_info() function
